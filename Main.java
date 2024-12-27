1c57aa1044898db258cd7bfa8e83448f8a27ed00
Titoputraaa <titobamulo13@gmail.com>
2024-12-27 22:58:41 +0700

    DONE, membuat method mencari nilia terbesar dengan algoritma linear

--- Main.java
+++ Main.java
@@ -13,14 +13,14 @@ public class Main {
 
         do{
             // starting menu 
-            System.out.println("====MENU====");
+            System.out.println("\n====MENU====");
             System.out.println("1 = Input data");
             System.out.println("2 = Menampilkan data");
             System.out.println("3 = Menampilkan nilai tertinggi");
             System.out.println("4 = keluar program");
             System.out.print("Pilih : ");
             int menu = scan.nextInt();
-            boolean dataAda = false;
+            System.out.println();
 
             switch (menu) {
                 case 1:
@@ -30,14 +30,21 @@ public class Main {
                     id = new String[totalData];
                     nama = new String[totalData];
                     nilai = new int[totalData];
-                    dataAda = true;
                     inputData(scan, id, nama, nilai);
                         break;
+
                 case 2 :
-                    showData(id, nama, nilai, dataAda);
+                    showData(id, nama, nilai);
                         break;
-                                
+
+                case 3 : 
+                    nilaiTertinggi(id, nama, nilai);
+                        break;
+                case 4 :
+                    System.out.println("keluar dari program");
+                    return;
                     default:
+                        System.out.println("masukan pilihan yang benar");
                         break;
             }
                     
@@ -47,23 +54,23 @@ public class Main {
 
         static void inputData(Scanner scan, String[] id, String[] nama, int[] nilai) {
             for (int i = 0; i < nama.length; i++) {
-                System.out.println("===MAHASISWA KE - " + (i+1));
+                System.out.println("MAHASISWA KE-" + (i+1));
                 System.out.print("ID : ");
                 id[i] = scan.next();
                 System.out.print("Nama : ");
-                nama[i] = scan.nextLine();
                 scan.nextLine();
-                System.out.print("ID : ");
+                nama[i] = scan.nextLine();
+                System.out.print("Nilai : ");
                 nilai[i] = scan.nextInt();
             }
         }
 
-        static void showData(String[] id, String[] nama, int[] nilai, boolean dataAda) {
-            if (dataAda) {
+        static void showData(String[] id, String[] nama, int[] nilai) {
+            if (nama.length != 0 ) {
                 for (int i = 0; i < nama.length; i++) {
-                    System.out.println("=== MAHASISWA KE - " + (i=1) + " ===");
+                    System.out.println("\nMAHASISWA KE-" + (i+1));
                     System.out.println("ID : " + id[i]);
-                    System.out.println("Nama: " + nama[i]);
+                    System.out.println("Nama : " + nama[i]);
                     System.out.println("Nilai : " + nilai[i]);
                 }
             } else {
@@ -73,14 +80,17 @@ public class Main {
  
         // note membuat algorima untuk memilih nilia tertinggi
         static void nilaiTertinggi(String[] id, String[] nama, int[] nilai){
-            for (int i = 0; i < nama.length; i++) {
+            for (int i = 0; i < 1; i++) {
+                int max = i;
                 for (int j = 0; j < nama.length; j++) {
-                    if (nilai[j] > nilai[i]) {
-                        int temp = nilai[j];
-                    }
+                    if (nilai[max] < nilai[j]) {
+                        max = j;
                 }
             }
+                System.out.println("Mahasiswa dengan nilai tertinggi");
+                System.out.println("ID : " + id[max]);
+                System.out.println("Nama : " + nama[max]);
+                System.out.println("Nilai  : " + nilai[max]);
         }
-
-        
+    }
 }
